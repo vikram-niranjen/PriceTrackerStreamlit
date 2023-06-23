4fb20a9a5942108f19d641d28bb4d3c4a260dca7
Vikram Niranjen <vikramniranjen@gmail.com>
2023-06-23 09:11:06 +0530

    portal: Added date filter

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import requests
 from datetime import datetime
 import pandas as pd
+import copy
 
 
 
@@ -17,10 +18,31 @@ if "product_id" in params:
     for record in records:
         temp = {}
         temp["Price"] = record["Price"]
-        temp["datetime"] = datetime.fromtimestamp(int(record["RowKey"]))
+        temp["DateTime"] = datetime.fromtimestamp(int(record["RowKey"]))
         price_history_records.append(temp)
     price_history_records = pd.DataFrame(price_history_records)
-    st.line_chart(data=price_history_records,  x="datetime", y="Price", )
+
+    min_date = price_history_records["DateTime"].min()
+    max_date = price_history_records["DateTime"].max()
+
+
+    start_date = copy.deepcopy(min_date)
+    end_date = copy.deepcopy(max_date)
+
+    date_col1, date_col2 = st.columns(2)
+    with date_col1:
+        start_date = st.date_input("Start Date",  value =min_date, min_value=min_date)
+
+    with date_col2:
+        end_date = st.date_input("End Date",  value= max_date,max_value=max_date)
+
+
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+
+
+    filtered = price_history_records.loc[(price_history_records["DateTime"]>=start_date)&(price_history_records["DateTime"]<=end_date)]
+    st.line_chart(data=filtered,  x="DateTime", y="Price", )
 
 
 
